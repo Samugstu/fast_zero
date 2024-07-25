a6ac7fb68c8e8d63d59e61251844099293ed53b7
samugstu <samuelsouza0225@gmail.com>
2024-07-25 10:00:08 -0300

    Segunda Aula

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI  # type: ignore
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_model=Message)
 def read_root():
   return {'message': 'Olár mundo'}
