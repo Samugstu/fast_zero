f51b2bdf5a6179517890919e161362952596cb28
samugstu <samuelsouza0225@gmail.com>
2024-07-31 20:56:10 -0300

    Cruds

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,3 +20,35 @@ def test_create_user(client):
     'email': 'teste@teste.com',
     'id': 1
     }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {'users': [{
+    'username': 'testeuser',
+    'email': 'teste@teste.com',
+    'id': 1
+    }]}
+
+
+def test_update_user(client):
+    response = client.put('/users/1',
+        json={
+            'password':'123',
+            'username': 'Samuca',
+            'email': 'samu@gmail.com',
+            'id': 1,
+        })
+    assert response.json() == {
+        'username': 'Samuca',
+        'email': 'samu@gmail.com',
+        'id': 1,
+    }
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted!'}
